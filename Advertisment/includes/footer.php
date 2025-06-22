accb03c250753124861a065a98afd7677c7d6f35
StevenY33T <143588962+StevenY33T@users.noreply.github.com>
2025-06-22 17:31:43 +0200

    Add files via upload

---
+++ Advertisment/includes/footer.php
@@ -0,0 +1,60 @@
+<footer class="footer">
+        <div class="container-fluid" id="foot">
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 row-cols-lg-5 g-4">
+                <div class="col mb-4" id="first">
+                    <h3>Website Name</h3>
+                    <p class="mt-3"><i class="bi bi-telephone me-2"></i> (000) 000 0000</p>
+                    <p><i class="bi bi-envelope me-2"></i> WebsiteName@gmail.com</p>
+                    <p><i class="bi bi-geo-alt me-2"></i> Address<br>Province, Postal Code</p>
+                </div>
+
+                <div class="col mb-4" id="second">
+                    <h4>Menu</h4>
+                    <ul>
+                        <li><a href="#">Home</a></li> 
+                        <li><a href="#">Page 1</a></li> 
+                        <li><a href="#">Page 2</a></li> 
+                        <li><a href="#">Page 3</a></li> 
+                        <li><a href="#">Page 4</a></li> 
+                    </ul>
+                </div>
+
+                <div class="col mb-4" id="second">
+                    <h4>Services</h4>
+                    <ul>
+                        <li><a href="#">Service 1</a></li> 
+                        <li><a href="#">Service 2</a></li> 
+                        <li><a href="#">Service 3</a></li> 
+                        <li><a href="#">Service 4</a></li> 
+                        <li><a href="#">Service 5</a></li> 
+                    </ul>
+                </div>
+                <div class="col mb-4" id="second">
+                    <h4>Resources</h4>
+                    <ul>
+                        <li><a href="#">Resource 1</a></li> 
+                        <li><a href="#">Resource 1</a></li> 
+                        <li><a href="#">Resource 1</a></li> 
+                        <li><a href="#">Resource 1</a></li> 
+                        <li><a href="#">Resource 1</a></li> 
+                    </ul>
+                </div>          
+                <div class="col mb-4" id="last">
+                    <h4>Connect With Us</h4>
+                    <p class="text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit provident amet nobis minima commodi iure ipsum facilis ipsam maxime numquam, velit, illo itaque. Culpa, aliquid distinctio molestiae nobis accusantium minus!</p>
+                    <div class="social-icons">
+                        <a href="#"><i class="bi bi-facebook"></i></a>
+                        <a href="#"><i class="bi bi-twitter"></i></a>
+                        <a href="#"><i class="bi bi-instagram"></i></a>
+                        <a href="#"><i class="bi bi-linkedin"></i></a>
+                        <a href="#"><i class="bi bi-youtube"></i></a>
+                    </div>
+                    
+                </div>
+            </div>
+            
+            <div class="copyright">
+                <p>&copy; 2025 Website Name. All rights reserved.</p>
+            </div>
+        </div>
+    </footer>
---
+++ Advertisment/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Advertising for Mauro's Dad</title>
+    <!-- Bootstrap 5 CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
+    <!-- Custom styles -->
+    <link rel="stylesheet" href="CSS/styles.css">
+        
+</head>
+<body>
+    <!-- Navigation -->
+    <?php require "includes/header.php" ?>
+
+    <!-- Main Body -->
+    <div class=container>
+     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi blanditiis nulla ea voluptate magni sunt possimus laborum quis voluptatum officiis vero corrupti asperiores culpa alias sequi odio, optio dolores incidunt?</p>
+     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi blanditiis nulla ea voluptate magni sunt possimus laborum quis voluptatum officiis vero corrupti asperiores culpa alias sequi odio, optio dolores incidunt?</p>
+     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi blanditiis nulla ea voluptate magni sunt possimus laborum quis voluptatum officiis vero corrupti asperiores culpa alias sequi odio, optio dolores incidunt?</p>
+     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi blanditiis nulla ea voluptate magni sunt possimus laborum quis voluptatum officiis vero corrupti asperiores culpa alias sequi odio, optio dolores incidunt?</p>
+     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi blanditiis nulla ea voluptate magni sunt possimus laborum quis voluptatum officiis vero corrupti asperiores culpa alias sequi odio, optio dolores incidunt?</p>
+     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi blanditiis nulla ea voluptate magni sunt possimus laborum quis voluptatum officiis vero corrupti asperiores culpa alias sequi odio, optio dolores incidunt?</p>
+     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi blanditiis nulla ea voluptate magni sunt possimus laborum quis voluptatum officiis vero corrupti asperiores culpa alias sequi odio, optio dolores incidunt?</p>
+     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi blanditiis nulla ea voluptate magni sunt possimus laborum quis voluptatum officiis vero corrupti asperiores culpa alias sequi odio, optio dolores incidunt?</p>
+     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi blanditiis nulla ea voluptate magni sunt possimus laborum quis voluptatum officiis vero corrupti asperiores culpa alias sequi odio, optio dolores incidunt?</p>
+     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi blanditiis nulla ea voluptate magni sunt possimus laborum quis voluptatum officiis vero corrupti asperiores culpa alias sequi odio, optio dolores incidunt?</p>
+     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi blanditiis nulla ea voluptate magni sunt possimus laborum quis voluptatum officiis vero corrupti asperiores culpa alias sequi odio, optio dolores incidunt?</p>
+     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi blanditiis nulla ea voluptate magni sunt possimus laborum quis voluptatum officiis vero corrupti asperiores culpa alias sequi odio, optio dolores incidunt?</p>
+     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi blanditiis nulla ea voluptate magni sunt possimus laborum quis voluptatum officiis vero corrupti asperiores culpa alias sequi odio, optio dolores incidunt?</p>
+     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi blanditiis nulla ea voluptate magni sunt possimus laborum quis voluptatum officiis vero corrupti asperiores culpa alias sequi odio, optio dolores incidunt?</p>
+     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi blanditiis nulla ea voluptate magni sunt possimus laborum quis voluptatum officiis vero corrupti asperiores culpa alias sequi odio, optio dolores incidunt?</p>
+    </div>
+    
+    <!-- Footer -->
+    <?php require "includes/footer.php" ?>
+
+    <!-- Bootstrap 5 JS Bundle -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
